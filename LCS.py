ee1fbc000859cb6587e12dcce7c95364b8ebd692
amirsalarabedini <abediniamirsalar@gmail.com>
2024-07-10 17:42:04 +0330

    fit:LCS

--- LCS.py
+++ LCS.py
@@ -6,17 +6,14 @@ def LCS(nums):
         if num - 1 not in num_set:
             current_num = num
             current_streak = 1
-
             while current_num + 1 in num_set:
                 current_num += 1
                 current_streak += 1
-
-            # بروز رسانی طولانی‌ترین دنباله
             longest_streak = max(longest_streak, current_streak)
 
     return longest_streak
 
-# مثال
+
 nums = [2, 3, 1, 255, 1, 155]
 result = LCS(nums)
-print(result)  # خروجی باید 3 باشد، زیرا طولانی‌ترین دنباله متوالی [1, 2, 3] است.
+print(result)  
