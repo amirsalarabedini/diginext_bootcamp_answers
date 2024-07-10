5a61ae25d298cc31f848425ed907f135a0520fed
amirsalarabedini <abediniamirsalar@gmail.com>
2024-07-10 17:24:29 +0330

    fit: search_matrix

---
+++ search_matrix.py
@@ -0,0 +1,32 @@
+def search_matrix(matrix, target):
+    if not matrix or not matrix[0]:
+        return False
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    left = 0
+    right = rows * cols - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        mid_value = matrix[mid // cols][mid % cols]
+
+        if mid_value == target:
+            return True
+        elif mid_value < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return False
+
+matrix = [
+    [1, 3, 5],
+    [8, 9, 15],
+    [12, 15, 18]
+]
+target = 8
+
+result = search_matrix(matrix, target)
+print(result) 
